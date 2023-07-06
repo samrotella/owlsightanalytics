d858f46167d83814e6418cb46956aa6886ccfc8f
samrotella <48039441+samrotella@users.noreply.github.com>
2023-07-06 14:44:36 -0400

    code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import _ from 'lodash';
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  // document.body.appendChild(component());
-  alert('hello world');
+
+let localStorageGuid = window.localStorage.getItem('owlGuid');
+
+if (localStorageGuid) {
+    analyze(localStorageGuid);
+}
+else {
+    let localGuid = createGuid();
+    window.localStorage.setItem('owlGuid', localGuid);
+    analyze(localGuid);
+}
+
+function createGuid() {
+    return 'aaaaaaaa-aaaa-4aaa-yaaa-aaaaaaaaaaaa'.replace(/[ay]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'a' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+// Need a way to pass up if this is a unique identifer or not
+function analyze(guid){
+    // navigator.userAgent
+}
