35115e134836e6051f2e592ab38c22e8ecd0729a
samrotella <48039441+samrotella@users.noreply.github.com>
2023-08-13 18:29:43 -0400

    no clue

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function createGuid() {
     });
 }
 
-function analyze(guid, conv){
+function analyze(guid){
   var MyDate = new Date();
   // var MyDateString;
 
@@ -37,7 +37,7 @@ function analyze(guid, conv){
         operatingSystem: navigator.platform,
         language: navigator.language,
         timeStamp: MyDate,
-        conversion: conv
+        // conversion: conv
       })
       .then(function (response) {
         console.log(response);
@@ -51,12 +51,12 @@ export function hello() {
   console.log('hello');
 }
 export function conversions(friendlyName, value) {
-  let guid = window.localStorage.getItem('owlGuid');
-  let convObj = {
-    name: friendlyName,
-    val: value
-  }
+  // let guid = window.localStorage.getItem('owlGuid');
+  // let convObj = {
+  //   name: friendlyName,
+  //   val: value
+  // }
 
-  analyze(guid, convObj);
+  // analyze(guid, convObj);
   console.log('conversions'); 
 }
